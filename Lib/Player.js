a2421b7314cc7c331ef26a4cb766d591b3b4c5ea
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-12-10 15:09:29 -0600

    check end of battle

--- Lib/Player.js
+++ Lib/Player.js
@@ -9,7 +9,7 @@ function Player(name = '') {
     this.health = Math.floor(Math.random() * 10 + 95);
     this.strength = Math.floor(Math.random() * 5 + 7);
     this.agility = Math.floor(Math.random() * 5 + 7);
-    this.inventory = [new Potion('health'), new Potion('agility')];
+    this.inventory = [new Potion('health'), new Potion()];
 
 
     //returns an object with the various player properties
@@ -63,7 +63,7 @@ function Player(name = '') {
     };
 
     Player.prototype.usePotion = function(index) {
-        const potion = this.getInventory().splice(index, 1)[0];
+        const potion = this.inventory.splice(index, 1)[0];
 
         switch (potion.name) {
             case 'agility':
